23c6fffab65ac6f4a906663bb1fe6a2be50e2421
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2020-11-19 15:57:07 -0600

    Creating derivation tree image without creating a dot file

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -25,20 +26,14 @@ func main() {
 
 	if matches {
 
-		// Save the graph
 		fmt.Printf("\x1b[1;32m%s is a valid semantic version\x1b[0m\n", word)
-		f, err := os.Create("tree.dot")
-		if err != nil {
-			log.Fatal("Failed creating file", err)
-		}
-		fmt.Fprint(f, graph)
-
-		// Make an image of the graph
+		// Save the graph
 		dotPath, err := exec.LookPath("dot")
 		if err != nil {
 			log.Fatal("graphviz not found")
 		}
-		cmd := exec.Command(dotPath, "-Tpng", "tree.dot", "-o", "tree.png")
+		cmd := exec.Command(dotPath, "-Tpng", "-o", "tree.png")
+		cmd.Stdin = strings.NewReader(graph.String())
 		err = cmd.Run()
 		if err != nil {
 			log.Fatal("Error runing graphviz", err)
